5a3f2cb4b6638e9f8c0a286c9b5ab51ba9efb3d4
Vishnu <mkv1722@gmail.com>
2023-05-23 15:49:52 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0B4F6C",
+      },
+    },
+  },
+  plugins: [],
+};
